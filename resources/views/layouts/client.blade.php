e1f30614913dee87fd6f3dcff767d46a9781be93
sawanascout <donne2339@gmail.com>
2025-06-27 04:59:49 +0100

    changement

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -177,8 +177,17 @@
                 <span class="fw-bold text-dark">GlobalDrop</span>
             </a>
 
-            
-<div class="collapse navbar-collapse" id="mainNavbar">
+            <button class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#mainNavbar"
+        aria-controls="mainNavbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse custom-collapse" id="mainNavbar">
                 <!-- Barre de recherche -->
                 <form action="{{ route('produits.index') }}" method="GET" class="my-2 d-flex mx-lg-3 my-lg-0 search-bar">
                     <input
