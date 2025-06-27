093828b49f63a4a77ffb04e9e8dac8b3b07c51f0
sawanascout <donne2339@gmail.com>
2025-06-27 02:30:19 +0100

    changement

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -6,6 +6,8 @@
     <title>GlobalDrop - @yield('title', 'Accueil')</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
     <script src="https://unpkg.com/lucide@latest"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
